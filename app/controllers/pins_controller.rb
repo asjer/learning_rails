f75f871f40743a0086dea59a596cf5dd1d8d7c35
Asjer Querido <asjer@johnyontherun.com>
2016-06-22 18:17:06 +0200

    added image upload with paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,46 +1,57 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show,]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   def index
-    @pins = Pin.all
+   @pins = Pin.all
   end
-
+  
   def show
   end
 
+  
   def new
     @pin = current_user.pins.build
   end
 
+
+  
   def edit
+
   end
 
+  
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.'
+        redirect_to @pin, notice: 'Pin was successfully created.'         
     else
-      render :new
+        render action: 'new' 
     end
   end
 
+  
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
+      redirect_to @pin, notice: 'Pin was successfully updated.' 
     else
-      render :edit
+     render action: 'edit'
     end
   end
 
+
+  
   def destroy
-    @pin.destroy
-    redirect_to pins_url
+    if @pin.destroy
+      redirect_to @pin, :notice => "Your post has been deleted"
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find_by(id: params[:id])
+      @pin = Pin.find(params[:id])
     end
 
     def correct_user
@@ -52,4 +63,4 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
-end
+  end
