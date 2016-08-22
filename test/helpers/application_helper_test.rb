d244d8da7128a38ee9ef0f8a366cb4f67684e8b1
Asjer Querido <asjer@johnyontherun.com>
2016-08-22 17:22:02 +0200

    finished layut and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,11 @@
+# require 'test_helper'
+
+# class ApplicationHelperTest < ActionView::TestCase
+#   test "full title helper" do
+#     assert_equal full_title("Home"), "title"
+#     assert_equal full_title("Help"), "title"
+#   end
+# end
+
+
+# moet nog naar kijken
